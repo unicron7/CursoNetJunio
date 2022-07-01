b417dc222edfcbff49fc645c6214f4a2d5cc5179
Luis Benitez <luiguibe90@hotmail.com>
2022-07-01 12:16:14 -0500

    Fase 3
    Benitez Luis Fase 3

---
+++ evaluacion-practica/Benitez Luis/Fase 3/Curso.Biblioteca/Curso.Biblioteca.WebApi/Program.cs
@@ -0,0 +1,30 @@
+using Curso.Biblioteca.Infraestructura;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<CursoBibliotecaDbContext>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
