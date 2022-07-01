01a8fbf2f512ca6438e1d76791384f0a5cb900a4
jgvalladares <valladaresgabriela2810@gmail.com>
2022-07-01 12:44:32 -0500

    Creacion de repositorios e interfaces

---
+++ evaluacion-practica/Gabriela Valladares/Curso.Biblioteca/Curso.Biblioteca.Dominio/Interfaces/IAutorRepo.cs
@@ -0,0 +1,17 @@
+﻿using Curso.Biblioteca.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso.Biblioteca.Dominio.Interfaces
+{
+    public interface IAutorRepo
+    {
+        IQueryable<Autor> GetAll();
+        Task CreateAsync(Autor autor);
+        Task UpdateAsync(Autor autor);
+        Task DeleteAsync(Autor autor);
+    }
+}
