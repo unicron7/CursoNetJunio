5cdcf2f28181d2e884398c06c3f6b543fb6d7ef9
sebassurkuna <sebassurkuna@gmail.com>
2022-07-01 13:54:58 -0500

    Fase7

---
+++ evaluacion-practica/Sebastian Santacruz/Fase7/Curso.Biblioteca/Curso.Biblioteca.Dominio/RepositorioInterfaz/IAutorRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Curso.Biblioteca.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso.Biblioteca.Dominio.RepositorioInterfaz
+{
+    public interface IAutorRepositorio
+    {
+        IQueryable<Autor> GetAll();
+        Task AddAsync (Autor autor);
+        Task UpdateAsync (Autor autor);
+        Task DeleteAsync (Autor autor);
+
+    }
+}
---
+++ evaluacion-practica/Sebastian Santacruz/Fase7/Curso.Biblioteca/Curso.Biblioteca.WebApi/Controllers/LibroController.cs
@@ -0,0 +1,49 @@
+﻿using Curso.Biblioteca.Aplicacion.ServicioInterfaz;
+using Curso.Biblioteca.Dominio.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso.Biblioteca.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase, ILibroServicio
+    {
+        private readonly ILibroServicio servicio;
+
+        public LibroController(ILibroServicio servicio)
+        {
+            this.servicio = servicio;
+        }
+
+        [HttpPost]
+        public Task<Libro> AddAsync(Libro libro)
+        {
+            return servicio.AddAsync(libro);
+        }
+
+        [HttpDelete]
+        public Task<bool> DeleteByIdAsync(int Id)
+        {
+            return servicio.DeleteByIdAsync(Id);
+        }
+
+        [HttpGet]
+        public Task<ICollection<Libro>> GetAllAsync()
+        {
+            return servicio.GetAllAsync();
+        }
+
+        [HttpGet("{Id}")]
+        public Task<Libro> GetByIdAsync(int Id)
+        {
+            return servicio.GetByIdAsync(Id);
+        }
+
+        [HttpPut]
+        public Task<Libro> UpdateAsync(Libro libro)
+        {
+            return servicio.UpdateAsync(libro);
+        }
+    }
+}
