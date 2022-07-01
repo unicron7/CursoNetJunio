b9f0a7fecd0a03a4a2641fbb5c62072c4ff45bbf
Luis Benitez <luiguibe90@hotmail.com>
2022-07-01 14:54:18 -0500

    Inyeccion

--- evaluacion-practica/Benitez Luis/Fase 7/Curso.Biblioteca/Curso.Biblioteca.WebApi/Program.cs
+++ evaluacion-practica/Benitez Luis/Fase 7/Curso.Biblioteca/Curso.Biblioteca.WebApi/Program.cs
@@ -1,4 +1,8 @@
+using Curso.Biblioteca.Aplicacion.Servicios_Definicion;
+using Curso.Biblioteca.Aplicacion.Servicios_Implementacion;
+using Curso.Biblioteca.Dominio.IRepositoriosDefinicion;
 using Curso.Biblioteca.Infraestructura;
+using Curso.Biblioteca.Infraestructura.RepositoriosImplementacion;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,13 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<CursoBibliotecaDbContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<IAutorRepositorio, AutorRepositorio>();
+builder.Services.AddTransient<IEditorialRepositorio, EditorialRepositorio>();
+builder.Services.AddTransient<IEditorialServicio, EditorialServicio>();
+builder.Services.AddTransient<ILibroRepositorio, LibroRepositorio>();
+builder.Services.AddTransient<ILibroServicio, LibroServicio>();
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
