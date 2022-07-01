b24baaf422163fc1dfd3af65a98539a85f9d0df1
Luis Benitez <luiguibe90@hotmail.com>
2022-07-01 14:12:58 -0500

    Controller

---
+++ evaluacion-practica/Benitez Luis/Fase 7/Curso.Biblioteca/Curso.Biblioteca.WebApi/Controllers/LibroController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso.Biblioteca.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+    }
+}
